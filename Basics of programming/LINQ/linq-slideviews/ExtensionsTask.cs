2b048319950db54db5baeb9870421762fdf247ac
Aleksandr <sashakamensk@mail.ru>
2019-05-28 19:01:24 +0300

    Complete ExtensionTask

---
+++ Basics of programming/LINQ/linq-slideviews/ExtensionsTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace linq_slideviews
+{
+    public static class ExtensionsTask
+    {
+        /// <summary>
+        /// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
+        /// Медиана списка из четного количества элементов — это среднее арифметическое 
+        /// двух серединных элементов списка после сортировки.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
+        public static double Median(this IEnumerable<double> items)
+        {
+            var enumerable = items.OrderBy(x => x).ToList();
+            if (!enumerable.Any()) throw new InvalidOperationException();
+
+            var amount = enumerable.Count();
+
+            var result = amount % 2 != 0
+                ? enumerable.ElementAt(amount / 2)
+                : (enumerable.ElementAt(amount / 2) + enumerable.ElementAt(amount / 2 - 1)) / 2.0;
+            return result;
+        }
+
+        /// <returns>
+        /// Возвращает последовательность, состоящую из пар соседних элементов.
+        /// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
+        /// </returns>
+        public static IEnumerable<Tuple<T, T>> Bigrams<T>(this IEnumerable<T> items)
+        {
+            var isFirst = true;
+            T prev = default(T);
+            foreach (var item in items)
+            {
+                if (!isFirst) yield return new Tuple<T, T>(prev, item);
+                prev = item;
+                isFirst = false;
+            }
+        }
+    }
+}
